3029b8f920a4c3134fcd9ed29aa1accad543b4f4
LorenzJHunter <lhunter1@iwu.edu>
2026-02-05 16:25:42 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,11 @@
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
+
+app.route("/")
+def index():
+    return render_template("index.html")
+
+app.route("/private")
+def private():
+    return render_template("private.html")
