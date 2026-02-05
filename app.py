523490b2ea49ef68f5860164d3a94aa048145620
LorenzJHunter <lhunter1@iwu.edu>
2026-02-05 16:34:46 -0600

    Create global data storage

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 
+USERPASS = {}
 app = Flask(__name__)
 
 app.route("/")
@@ -10,4 +11,5 @@ app.route("/private", methods=["GET"])
 def private():
     user = request.args.get("username")
     password = request.args.get("password")
-    return render_template("private.html", user=user, password=password)
+    USERPASS[user] = password
+    return render_template("private.html", userpass=USERPASS)
